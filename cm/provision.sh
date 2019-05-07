3e1e90dcde1c27186e9a80769bea6ff75c7bb8e0
jiahua <jiahua.xu@outlook.com>
2019-05-07 14:09:02 +0800

    cm

--- cm/provision.sh
+++ cm/provision.sh
@@ -13,3 +13,19 @@ hostnamectl set-hostname cm.example.com
 echo -e "192.168.33.10  cm.example.com\n192.168.33.11  node01.example.com\n192.168.33.12  node02.example.com\n192.168.33.13  node03.example.com\n" | tee -a /etc/hosts
 mkdir -p /etc/sysconfig
 echo -e "HOSTNAME=cm.example.com\n" > /etc/sysconfig/network
+
+# Step 1: Configure a Repository for Cloudera Manager
+cd /etc/apt/sources.list.d/ && wget https://archive.cloudera.com/cm6/6.2.0/ubuntu1604/apt/cloudera-manager.list
+wget https://archive.cloudera.com/cm6/6.2.0/ubuntu1604/apt/archive.key
+apt-key add archive.key
+apt-get update
+
+# Step 2: Install Java Development Kit
+apt-get install -y oracle-j2sdk1.8
+
+# Step 3: Install Cloudera Manager Server
+apt-get install cloudera-manager-daemons cloudera-manager-agent cloudera-manager-server
+
+# Step 4: Install and Configure Databases
+# apt-get install mysql-server
+# service mysql stop
