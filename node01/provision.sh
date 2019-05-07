d90e6bf47e5f190a2b0ba7e13d76c4d7794aa08a
jiahua <jiahua.xu@outlook.com>
2019-05-07 15:55:09 +0800

    node provision

--- node01/provision.sh
+++ node01/provision.sh
@@ -1,11 +1,20 @@
+
+yum install -y wget
+
+# Setting SELinux mode
+sed -i 's/SELINUX=enforcing/SELINUX=permissive/g' /etc/selinux/config
+setenforce 0
+
 # Disabling the Firewall
-service ufw stop
+iptables-save > ~/firewall.rules
+systemctl disable firewalld
+systemctl stop firewalld
 
 # Enable an NTP Service
-apt-get install -y ntp ntpdate
+yum install -y ntp
 echo -e "server 0.pool.ntp.org\nserver 1.pool.ntp.org\nserver 2.pool.ntp.org\n" > /etc/ntp.conf
-systemctl start ntp
-systemctl enable ntp
+systemctl start ntpd
+systemctl enable ntpd
 hwclock --systohc
 
 # Configure Network Names
@@ -13,3 +22,10 @@ hostnamectl set-hostname node01.example.com
 echo -e "192.168.33.10  cm.example.com\n192.168.33.11  node01.example.com\n192.168.33.12  node02.example.com\n192.168.33.13  node03.example.com\n" | tee -a /etc/hosts
 mkdir -p /etc/sysconfig
 echo -e "HOSTNAME=node01.example.com\n" > /etc/sysconfig/network
+
+# Step 1: Configure a Repository for Cloudera Manager
+wget https://archive.cloudera.com/cm6/6.2.0/redhat7/yum/cloudera-manager.repo -P /etc/yum.repos.d/
+rpm --import https://archive.cloudera.com/cm6/6.2.0/redhat7/yum/RPM-GPG-KEY-cloudera
+
+# Step 2: Install Java Development Kit
+yum install -y oracle-j2sdk1.8
